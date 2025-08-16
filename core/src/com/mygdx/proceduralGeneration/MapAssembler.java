0c7a72a070042031157d5f7333daf4390049d4d2
Isaac <isaacfiuzavieira@gmail.com>
2025-08-16 20:00:57 -0300

    feat: Mansion art

--- core/src/com/mygdx/proceduralGeneration/MapAssembler.java
+++ core/src/com/mygdx/proceduralGeneration/MapAssembler.java
@@ -31,16 +31,55 @@ public class MapAssembler {
         for (int i = 0; i < grade.length; i++) {
             for (int j = 0; j < grade[i].length; j++) {
                 if (grade[i][j] == 0 || grade[i][j] == 2) { // chao ou porta
-                    map[i][j] = new TileMap(asset.getSpriteFromAtlas("MansionTiles", "Piso" + rand.nextInt(1, 4)), new Vector2(32 * i, 32 * j), false);
+                    map[i][j] = new TileMap(asset.getSpriteFromAtlas("MansionTiles", "Piso" + generatePiso()), new Vector2(32 * i, 32 * j), false);
 
-                } else if (grade[i][j] == 1) { // parede
-                    map[i][j] = new TileMap(asset.getSpriteFromAtlas("MansionTiles", "Parede"), new Vector2(32 * i, 32 * j), true);
+                } else if(grade[i][j] == 1){
+
+                    if(j > 0 && grade[i][j-1] != 1){
+                        map[i][j] = new TileMap(asset.getSpriteFromAtlas("MansionTiles", "ParedeFrontal" + generateParede()), new Vector2(32 * i, 32 * j), true);
+
+                    } else {
+                        map[i][j] = new TileMap(asset.getSpriteFromAtlas("MansionTiles", "Parede"), new Vector2(32 * i, 32 * j), true);
+                    }
+
                     walls.add(map[i][j]);
                 }
             }
         }
 
         return map;
+    }
+
+    public int generatePiso() {
+        float[] probabilidades = {0.26f, 0.26f, 0.26f, 0.164f, 0.055f, 0.001f}; // Soma deve ser 1
+
+        float num = rand.nextFloat();
+        float acumulado = 0;
+
+        for (int i = 0; i < probabilidades.length; i++) {
+            acumulado += probabilidades[i];
+            if (num <= acumulado) {
+                return i + 1;
+            }
+        }
+
+        return 1;
+    }
+
+    public int generateParede(){
+        float[] probabilidades = {0.69f, 0.001f, 0.105f, 0.104f, 0.10f}; // Soma deve ser 1
+
+        float num = rand.nextFloat();
+        float acumulado = 0;
+
+        for (int i = 0; i < probabilidades.length; i++) {
+            acumulado += probabilidades[i];
+            if (num <= acumulado) {
+                return i + 1;
+            }
+        }
+
+        return 1;
     }
 
     public Array<TileMap> getWalls(){ return walls; }
